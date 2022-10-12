fe4aaa3eacd46457f4f0d2893c48329a65f657ec
dmt130 <derciomichaque@gmail.com>
2022-10-12 13:10:21 +0200

    Serveless application last commit

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -8,7 +8,7 @@ import { JwtPayload } from '../../auth/JwtPayload'
 const logger = createLogger('auth');
 
 // Todo
-const jwksUrl = '';
+const jwksUrl = 'https://dev-192ioslt.us.auth0.com/.well-known/jwks.json';
 
 
 export const handler = async (
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '';
+const apiId = 'vl9jw1vx95';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '',
-  clientId: '',
+  domain: 'dev-192ioslt.us.auth0.com',
+  clientId: 'keN9l5Mv3Fo3XLHRzRCRnNLmJMftOLfz',
   callbackUrl: 'http://localhost:3000/callback'
 };
